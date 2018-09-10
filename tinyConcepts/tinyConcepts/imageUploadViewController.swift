24108af6b598f3e7843e2feb3d985e1eaaae2bd8
codyowl <codyowl@gmail.com>
2018-09-10 17:59:42 +0530

    Image uploader added :heavy_plus_sign:

---
+++ tinyConcepts/tinyConcepts/imageUploadViewController.swift
@@ -0,0 +1,28 @@
+//
+//  imageUploadViewController.swift
+//  tinyConcepts
+//
+//  Created by Tringapps-Admin on 10/09/18.
+//  Copyright © 2018 Tringapps-Admin. All rights reserved.
+//
+
+import UIKit
+
+class imageUploadViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func uploadButtonPressed(_ sender: Any) {
+        let uploader  = UIImagePickerController()
+     
+        // to present our uploader
+        present(uploader, animated: true, completion: nil)
+    }
+    
+
+}
