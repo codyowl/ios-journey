7435df0e06b23c891c0248e9d0069707ee353fe8
codyowl <codyowl@gmail.com>
2018-09-17 15:03:51 +0530

    Split view introduction removed to add it again :pencil2:

--- ScrollViewUnderstanding/ScrollViewUnderstanding/ViewController.swift
+++ ScrollViewUnderstanding/ScrollViewUnderstanding/ViewController.swift
@@ -18,20 +18,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // creating an image view to represent along with the scroll image
+        // create an image view to represent along with the scroll image
         
         imageView = UIImageView(image: UIImage(named: "Image"))
        
-        // Creating our scroll view with the frame parameter
+        // Create a scroll view with the frame parameter
         scrollView = UIScrollView(frame: view.bounds)
         scrollView.backgroundColor = UIColor.black
         
         scrollView.contentSize = imageView.bounds.size
         
-        // adding the image view (which is our image) as a subview to the scroll view using addSubview
+        // add the image view (which is our image) as a subview to the scroll view using addSubview
         scrollView.addSubview(imageView)
         
-        // adding the scroll view as a subview to the view
+        // add the scroll view as a subview to the view
         view.addSubview(scrollView)
     }
 
